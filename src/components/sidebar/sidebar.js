0fe85ab0df0f54409b0916aa96c65d9bf193daf7
Oscar Ortiz <fabioc.fo64@gmail.com>
2022-09-07 23:31:15 -0600

    Changes on navbar css

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,47 +1,49 @@
 import React, { useState } from "react"
 export default function Sidebar(){
+    const [hidden, setHidden] = useState(false)
     return (
-    <aside class="w-64" aria-label="Sidebar">
-        <div class="overflow-y-auto min-h-full py-4 px-3 bg-blue-dark">
-            <ul class="space-y-2">
-                <a href="https://flowbite.com/" class="flex items-center p-2 text-base font-normal text-mercury-600 rounded-lg">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-7" alt="Flowbite Logo" />
-                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Taskademic Board</span>
-                </a>
-                <li>
-                    <a href="#" class="flex items-left p-2 text-base font-normal text-mercury-600 rounded-lg hover:bg-bright-turquoise-700">
-                    <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-mercury-50 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
-                    <span class="ml-3">Manage my tasks on the current period</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="flex items-left p-2 text-base font-normal text-mercury-600 rounded-lg hover:bg-bright-turquoise-700">
-                    <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-mercury-50 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
-                    <span class="flex-1 ml-3 whitespace-nowrap">Create new college period</span>
-                    <span class="inline-flex justify-center items-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="flex items-left p-2 text-base font-normal text-mercury-600 rounded-lg hover:bg-bright-turquoise-700">
-                    <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-mercury-50 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
-                    <span class="flex-1 ml-3 whitespace-nowrap">Check past college periods</span>
-                    <span class="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-blue-600 bg-blue-200 rounded-full dark:bg-blue-900 dark:text-blue-200">3</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="flex items-left p-2 text-base font-normal text-mercury-600 rounded-lg hover:bg-bright-turquoise-700">
-                    <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-mercury-50 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-                    <span class="flex-1 ml-3 whitespace-nowrap">Manage my account</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="flex items-left p-2 text-base font-normal text-mercury-600 rounded-lg hover:bg-bright-turquoise-700">
-                    <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-mercury-50 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
-                    <span class="flex-1 ml-3 whitespace-nowrap">About</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </aside>
+        <aside className="w-64" aria-label="Sidebar">
+            <div className="relative">
+                <button type="button" className="absolute top-0 -right-0 text-mercury-600">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </button>
+            </div>
+            <div className="overflow-y-auto min-h-full py-4 px-3 bg-blue-dark">
+                <ul className="space-y-2">
+                    <li className="border-b border-mercury-600">
+                        <a href="/" className="flex items-center py-4 text-base text-center font-normal text-mercury-600">
+                            <span className="self-center text-xl flex-auto font-semibold dark:text-white">Taskademic Board</span>
+                        </a>
+                    </li>
+                    <li className="border-b border-mercury-600">
+                        <a href="#" className="flex items-center py-2 text-base text-center font-normal text-mercury-600 rounded-lg hover:bg-bright-turquoise-700">
+                        <span className="ml-3">Manage my tasks on the current period</span>
+                        </a>
+                    </li>
+                    <li className="border-b border-mercury-600">
+                        <a href="#" className="flex items-center py-2 text-base text-center font-normal text-mercury-600 rounded-lg hover:bg-bright-turquoise-700">
+                        <span className="flex-1 ml-3 whitespace-nowrap">Create new college period</span>
+                        </a>
+                    </li>
+                    <li className="border-b border-mercury-600">
+                        <a href="#" className="flex items-center py-2 text-base text-center font-normal text-mercury-600 rounded-lg hover:bg-bright-turquoise-700">
+                        <span className="flex-1 ml-3 whitespace-nowrap">Check past college periods</span>
+                        </a>
+                    </li>
+                    <li className="border-b border-mercury-600">
+                        <a href="#" className="flex items-center py-2 text-base text-center font-normal text-mercury-600 rounded-lg hover:bg-bright-turquoise-700">
+                        <span className="flex-1 ml-3 whitespace-nowrap">Manage my account</span>
+                        </a>
+                    </li>
+                    <li className="border-b border-mercury-600">
+                        <a href="#" className="flex items-center py-2 text-base text-center font-normal text-mercury-600 rounded-lg hover:bg-bright-turquoise-700">
+                        <span className="flex-1 ml-3 whitespace-nowrap">About</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </aside>
     )
 }
