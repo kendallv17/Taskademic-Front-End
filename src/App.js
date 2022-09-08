f68807c7c25cc3256f303fb05157271e4f9f578e
Oscar Ortiz <fabioc.fo64@gmail.com>
2022-09-07 22:50:32 -0600

    Initial iteration on navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Sidebar from "./components/sidebar/sidebar";
 function App() {
   return (
-    <div className="flex min-h-full">
+    <div className="flex min-h-screen">
       <Sidebar></Sidebar>
       <div className="h-full flex-auto">1</div>
     </div>
