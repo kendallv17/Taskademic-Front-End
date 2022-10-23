c1459ac8cb8515a1ad8ee1d155530bf698b3e381
Oscar Ortiz <fabioc.fo64@gmail.com>
2022-10-22 22:07:38 -0600

    Finished landing page

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
-import { Accordion } from "flowbite-react";
 import landing from "../../images/landing.jpg"
+import students from "../../images/students.png"
 export default function Landing(){
     return (
         <div className="text-white w-screen h-full" style={{
             background: 'linear-gradient( 65.5deg,  rgba(23,205,205,1) -15.1%, rgba(23,25,95,1) 71.5% )',
             font:'Source Sans Pro, sans-serif'
         }}>
-		<div className="container px-3 flex flex-wrap flex-col md:ml-auto md:flex-row items-center">
-			<div className="flex-1 flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
+		<div className="container px-3 flex flex-wrap flex-col md:ml-auto md:flex-row items-center rounded-lg">
+			<div className="flex-1 flex-col w-full md:w-2/5 justify-center items-center text-center md:text-left">
 				<p className="uppercase tracking-loose w-full">Are you having troubles organizing your academic asigments?</p>
 				<h1 className="my-4 text-5xl font-bold leading-tight">Taskademic is here to help you!</h1>
 				<p className="leading-normal text-2xl mb-8">We help you to keep track of assigned tasks so that you can focus on working</p>
@@ -18,20 +18,17 @@ export default function Landing(){
 				<img className="w-full md:w-4/5 z-50 rounded-lg" alt="Board managing"src={landing}/>
 			</div>
 		</div>
-        <section className="w-full">
-        <Accordion alwaysOpen={true} class="bg-mercury-500 text-blue-dark-500">
-            <Accordion.Panel>
-                <Accordion.Title>
-                    What is taskademic?
-                </Accordion.Title>
-                    <Accordion.Content>
-                    <p className="mb-2">
-                        Taskademic is a web based application that allow you to manage your academic tasks. It is aimed for students of all ages that are having a bad time keeping up with ther assignments
-                    </p>
-                    </Accordion.Content>
-            </Accordion.Panel>
-        </Accordion>
-        </section>
+        <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center">What is taskademic?</h1>
+        <div className="px-3 flex flex-wrap flex-col md:ml-auto md:flex-row items-center rounded-lg">
+            <div className="flex-1 flex-col w-full md:w-2/5 justify-center items-center text-center md:text-left">
+                <h5 className="leading-normal text-2xl mb-8">Taskademic is a web based application that
+                    allow you to manage your academic tasks. It is aimed for students of all ages that are having a bad time keeping up with their assignments
+                </h5>
+            </div>
+            <div className="flex-1 w-full md:w-3/5 py-6 text-center">
+                <img className="w-full md:w-4/5 z-50 rounded-lg" src={students} alt="Students"/>
+            </div>
+        </div>
 	</div>
     );
 }
